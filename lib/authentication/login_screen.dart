99cf66001e5dbe83890a35091b5a3cb396ff3377
Amaan <amaanaliakhtar@gmail.com>
2023-11-20 12:05:24 +0000

    Login screen design complete

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -16,10 +16,63 @@ class LoginScreen extends StatelessWidget {
               children: [
                 Image(
                   image: AssetImage('assets/login-image.png'),
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                 ),
                 const Text("Welcome!"),
                 const Text("Dontate where you want, when you want."),
+
+                //Form
+                Form(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.person_outline_outlined),
+                              labelText: "Email",
+                              hintText: "Email",
+                              border: OutlineInputBorder()),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.fingerprint),
+                              suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                              labelText: "Password",
+                              hintText: "Password",
+                              border: OutlineInputBorder()),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text("Forgot password?"),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {}, child: const Text("LOGIN")),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Don't have an account?"),
+                            TextButton(
+                                onPressed: () {}, child: const Text("Register"))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
