5bf9933f12a3da2eaa34066d14fdd350b20205e8
Amaan <amaanaliakhtar@gmail.com>
2024-01-30 11:35:15 +0000

    Added search bar and card section

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -11,7 +11,7 @@ class Dashboard extends StatelessWidget {
           Icons.menu,
           color: Colors.black,
         ),
-        title: Text(
+        title: const Text(
           "Charity Navigator",
         ),
         centerTitle: true,
@@ -20,15 +20,79 @@ class Dashboard extends StatelessWidget {
         actions: [
           Container(
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Image(image: AssetImage("assets/logo.png")),
+            ),
           )
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20),
+          child: Column(
+            //stack children vertically
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Hey!"),
+              const Text("Explore Charities"),
+              const SizedBox(
+                height: 20,
+              ),
+
+              //search bar
+              Container(
+                decoration: (const BoxDecoration(
+                    border: Border(left: BorderSide(width: 3)))),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Search..."),
+                    Icon(
+                      Icons.search,
+                      size: 24,
+                    )
+                  ], //todo change opacity
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              //filter options
+              SizedBox(
+                width: 170,
+                height: 50,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0x212833),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "E",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+//TODO
+//change fonts and text size
